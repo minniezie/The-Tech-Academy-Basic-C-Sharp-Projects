585f6e0030c7566d5e29db5d0afef8ff2c007b10
minniezie <minni.wadhwa9@gmail.com>
2025-04-14 12:41:28 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace My_New_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting System");
+            Console.WriteLine("Remember, we're 'accounting' on you");
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
